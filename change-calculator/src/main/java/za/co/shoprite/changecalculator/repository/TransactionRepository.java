20bb2e374a9aee8a077e45713c6ba8322a42f148
shannonweir <shannonweir02@gmail.com>
2024-02-27 18:17:59 +0200

    Initial Commit

--- change-calculator/src/main/java/za/co/shoprite/changecalculator/repository/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package za.co.shoprite.changecalculator.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import za.co.shoprite.changecalculator.domain.Transaction;
-
-//@Repository
-public interface TransactionRepository extends JpaRepository<Transaction, String> {
-}
